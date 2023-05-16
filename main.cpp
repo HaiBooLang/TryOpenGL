3ad6950b0d4c7cd549abfbc5aafceabd27319fd6
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-05-16 15:19:19 +0800

    功能 - 透明窗口 - 1

--- main.cpp
+++ main.cpp
@@ -1,86 +1,86 @@
-#include <glad/glad.h>
+// https://ecode.dev/transparent-framebuffer-borderless-window-using-glfw/
+#include <windows.h>
 #include <GLFW/glfw3.h>
 
-#include <iostream>
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow* window);
-
-// settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
-
-int main()
+// change this to int WINAPI WinMain() to avoid the console
+int main(HINSTANCE hInstance, HINSTANCE hPrevInstance, char*, int nShowCmd)
 {
-	// glfw: initialize and configure
-	// ------------------------------
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    GLFWwindow* window;
+    int windowSizeW = 640, windowSizeH = 480;
+    // initialize the library
+    if (!glfwInit())
+        return -1;
 
-#ifdef __APPLE__
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
+    glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE);
+    int count, windowWidth, windowHeight, monitorX, monitorY;
 
-	// glfw window creation
-	// --------------------
-	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "HaiBooLang", nullptr, nullptr);
-	if (window == nullptr)
-	{
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent(window);
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    // set the position of the window
+    // I am assuming that main monitor is in the 0 position
+    GLFWmonitor** monitors = glfwGetMonitors(&count);
+    const GLFWvidmode* videoMode = glfwGetVideoMode(monitors[0]);
+    // width: 75% of the screen
+    windowWidth = static_cast<int>(videoMode->width / 1.5);
+    // aspect ratio 16 to 9
+    windowHeight = static_cast<int>(videoMode->height / 16 * 9);
 
-	// glad: load all OpenGL function pointers
-	// ---------------------------------------
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
-		std::cout << "Failed to initialize GLAD" << std::endl;
-		return -1;
-	}
+    glfwGetMonitorPos(monitors[0], &monitorX, &monitorY);
 
-	// render loop
-	// -----------
-	while (!glfwWindowShouldClose(window))
-	{
-		// input
-		// -----
-		processInput(window);
+    // set the visibility window hint to false for subsequent window creation
+    // glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
 
-		// render
-		// ------
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+    // create a windowed mode window and its OpenGL context
+    window = glfwCreateWindow(windowSizeW, windowSizeH, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
 
-		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-		// -------------------------------------------------------------------------------
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
+    // make the window's context current
+    glfwMakeContextCurrent(window);
 
-	// glfw: terminate, clearing all previously allocated GLFW resources.
-	// ------------------------------------------------------------------
-	glfwTerminate();
-	return 0;
-}
+    // reset the window hints to default
+    glfwDefaultWindowHints();
 
-// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-// ---------------------------------------------------------------------------------------------------------
-void processInput(GLFWwindow* window)
-{
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, true);
-}
+    glfwSetWindowPos(window,
+        monitorX + (videoMode->width - windowWidth) / 2,
+        monitorY + (videoMode->height - windowHeight) / 2);
 
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-	// make sure the viewport matches the new window dimensions; note that width and 
-	// height will be significantly larger than specified on retina displays.
-	glViewport(0, 0, width, height);
+    // show the window
+    glfwShowWindow(window);
+
+    // uncomment following line to see the border of window
+    glfwSetWindowAttrib(window, GLFW_DECORATED, GLFW_FALSE);
+
+    float fAngle = 0.0f;
+    // Loop until the user closes the window
+    while (!glfwWindowShouldClose(window))
+    {
+        // render
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glLoadIdentity();
+        glRotatef(fAngle, 0, 0, 1);
+        glBegin(GL_QUADS);
+        glColor3f(0, 0, 1);
+        glVertex3f(-0.5, -0.5, -1);
+        glColor3f(0, 1, 0);
+        glVertex3f(0.5, -0.5, -1);
+        glColor3f(1, 0, 1);
+        glVertex3f(0.5, 0.5, -1);
+        glColor3f(1, 1, 0);
+        glVertex3f(-0.5, 0.5, -1);
+        glEnd();
+
+        fAngle += 0.01;  // at some point this will explote! overflow: put the corresponding of
+
+        // swap front and back buffers
+        glfwSwapBuffers(window);
+
+        // poll for and process events
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
