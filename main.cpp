6c1b3bb25dbd5d2d0ee5c2fc2541802c95c5e504
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-05-17 22:42:14 +0800

    1. 换个顺序
    使用应用在箱子上的最后一个变换，尝试将其改变为先旋转，后位移。看看发生了什么，试着想想为什么会发生这样的事情：[参考解答](https://learnopengl.com/code_viewer.php?code=getting-started/transformations-exercise1)

--- main.cpp
+++ main.cpp
@@ -167,8 +167,8 @@ int main()
 
         // create transformations
         glm::mat4 transform = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
-        transform = glm::translate(transform, glm::vec3(0.5f, -0.5f, 0.0f));
-        transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+        transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f)); // Switched the order
+        transform = glm::translate(transform, glm::vec3(0.5f, -0.5f, 0.0f)); // Switched the order     
 
         // get matrix's uniform location and set matrix
         ourShader.use();
