755dc367e463da6ea3a29381f7bd60a0fcf068cf
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-05-20 22:00:39 +0800

    光照 - 材质 - 2

--- resource/shader/colors.fs
+++ resource/shader/colors.fs
@@ -11,28 +11,34 @@ struct Material {
     float shininess;
 }; 
 
-uniform Material material;
-uniform vec3 viewPos; 
-uniform vec3 lightColor;
-uniform vec3 lightPos;
+struct Light {
+    vec3 position;
 
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+uniform vec3 viewPos; 
+uniform Material material;
+uniform Light light;
 
 void main()
 {   
     // diffuse reflection
     vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(lightPos - FragPos);
+    vec3 lightDir = normalize(light.position - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = lightColor * (diff * material.diffuse);
+    vec3 diffuse = light.diffuse * (diff * material.diffuse);
 
     // ambient lighting
-    vec3 ambient = lightColor * material.ambient;
+    vec3 ambient  = light.ambient * material.ambient;
 
     // specular reflection
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);  
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-    vec3 specular = lightColor * (spec * material.specular);  
+    vec3 specular = light.specular * (spec * material.specular);  
 
     vec3 result = ambient + diffuse + specular;
     FragColor = vec4(result, 1.0);
--- source/main.cpp
+++ source/main.cpp
@@ -206,6 +206,11 @@ int CALLBACK WinMain(
 		lightingShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
 		lightingShader.setFloat("material.shininess", 32.0f);
 
+		lightingShader.setVec3("light.position", lightPos.x, lightPos.y, 2.0f);
+		lightingShader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
+		lightingShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
+		lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+
 		// view/projection transformations
 		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 		glm::mat4 view = camera.GetViewMatrix();
