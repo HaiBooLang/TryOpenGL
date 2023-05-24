e87a3bd95e0b298569bcc8ede894abc2d3815cd2
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-05-24 23:31:24 +0800

    高级OpenGL - 帧缓冲 - 4

--- resource/shader/framebuffers_screen.fs
+++ resource/shader/framebuffers_screen.fs
@@ -8,6 +8,6 @@ uniform sampler2D screenTexture;
 void main()
 {
     FragColor = texture(screenTexture, TexCoords);
-    float average = (FragColor.r + FragColor.g + FragColor.b) / 3.0;
+    float average = 0.2126 * FragColor.r + 0.7152 * FragColor.g + 0.0722 * FragColor.b;
     FragColor = vec4(average, average, average, 1.0);
 }
